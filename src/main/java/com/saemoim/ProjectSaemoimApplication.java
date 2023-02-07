c08932c86ef5f6d6bb5d4b22bb40d89f9c56d65d
seongjun <seongjuni1@naver.com>
2023-02-07 22:06:51 +0900

    first commit

---
+++ src/main/java/com/saemoim/ProjectSaemoimApplication.java
@@ -0,0 +1,13 @@
+package com.saemoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSaemoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSaemoimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saemoim/ProjectSaemoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saemoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSaemoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
