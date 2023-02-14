7f62ff5f7ebb160a1012f9db16206fa258ae7ff9
SongeonLee <qkfka1040@icloud.com>
2023-02-14 22:22:03 +0900

    [test] ReviewService test #29

--- src/main/java/com/saemoim/domain/Review.java
+++ src/main/java/com/saemoim/domain/Review.java
@@ -10,11 +10,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Review extends TimeStamped {
 	@Id
