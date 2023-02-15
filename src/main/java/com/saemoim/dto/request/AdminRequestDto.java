b78dba7aefbff826fa8580bfb4dec3c421155b19
SongeonLee <qkfka1040@icloud.com>
2023-02-15 20:41:11 +0900

    Update AdminRequestDto.java

--- src/main/java/com/saemoim/dto/request/AdminRequestDto.java
+++ src/main/java/com/saemoim/dto/request/AdminRequestDto.java
@@ -12,7 +12,7 @@ public class AdminRequestDto {
 	@Size(min = 2, max = 10)
 	private String username;
 	@Size(min = 8, max = 15)
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$", message = "알파벳, 숫자, 특수문자(!@#$%^&*())가 한 개 이상 포함되어야 합니다.")
 	private String password;
 
 	@Builder
