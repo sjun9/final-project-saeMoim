55c41a37873ba57c07472cccffe5be3ca50b9738
SongeonLee <qkfka1040@icloud.com>
2023-02-15 14:41:32 +0900

    [Feat] 모임탈퇴기능 #39

--- src/main/java/com/saemoim/service/ParticipantServiceImpl.java
+++ src/main/java/com/saemoim/service/ParticipantServiceImpl.java
@@ -1,6 +1,5 @@
 package com.saemoim.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,10 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.saemoim.domain.Group;
 import com.saemoim.domain.Participant;
+import com.saemoim.domain.User;
 import com.saemoim.dto.response.ParticipantResponseDto;
 import com.saemoim.exception.ErrorCode;
 import com.saemoim.repository.GroupRepository;
 import com.saemoim.repository.ParticipantRepository;
+import com.saemoim.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +22,7 @@ public class ParticipantServiceImpl implements ParticipantService {
 
 	private final ParticipantRepository participantRepository;
 	private final GroupRepository groupRepository;
+	private final UserRepository userRepository;
 
 	@Transactional(readOnly = true)
 	@Override
@@ -28,15 +30,22 @@ public class ParticipantServiceImpl implements ParticipantService {
 		Group group = groupRepository.findById(groupId).orElseThrow(
 			() -> new IllegalArgumentException(ErrorCode.NOT_FOUND_GROUP.getMessage())
 		);
-		List<ParticipantResponseDto> participantResponseDto = new ArrayList<>();
 		List<Participant> participants = participantRepository.findAllByGroupOrderByCreatedAtDesc(group);
-		participants.forEach(participant -> participantResponseDto.add(new ParticipantResponseDto(participant)));
-		return participantResponseDto;
+		return participants.stream().map(ParticipantResponseDto::new).toList();
 	}
 
 	@Transactional
 	@Override
-	public void withdrawGroup(Long participantId, Long userId) {
-
+	public void withdrawGroup(Long groupId, Long userId) {
+		Group group = groupRepository.findById(groupId).orElseThrow(
+			() -> new IllegalArgumentException(ErrorCode.NOT_FOUND_GROUP.getMessage())
+		);
+		User user = userRepository.findById(userId).orElseThrow(
+			() -> new IllegalArgumentException(ErrorCode.NOT_FOUND_USER.getMessage())
+		);
+		Participant participant = participantRepository.findByGroupAndUser(group, user).orElseThrow(
+			() -> new IllegalArgumentException(ErrorCode.NOT_FOUND_PARTICIPANT.getMessage())
+		);
+		participantRepository.delete(participant);
 	}
 }
