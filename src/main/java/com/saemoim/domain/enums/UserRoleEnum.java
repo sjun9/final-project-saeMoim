57a74e44a97ada7923d6f5638a03e92a32085ef6
SongeonLee <qkfka1040@icloud.com>
2023-02-15 09:34:50 +0900

    Update UserRoleEnum.java

--- src/main/java/com/saemoim/domain/enums/UserRoleEnum.java
+++ src/main/java/com/saemoim/domain/enums/UserRoleEnum.java
@@ -15,8 +15,6 @@ public enum UserRoleEnum {
 
 	public static class Authority {
 		public static final String USER = "ROLE_USER";
-		public static final String LEADER = "ROLE_LEADER";
-		public static final String PARTICIPANT = "ROLE_PARTICIPANT";
 		public static final String REPORT = "ROLE_REPORT";
 		public static final String ADMIN = "ROLE_ADMIN";
 	}
