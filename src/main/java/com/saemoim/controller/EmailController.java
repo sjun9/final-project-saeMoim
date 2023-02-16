551e688844e9b9f761e260c4ec73a942b904b3b3
SongeonLee <qkfka1040@icloud.com>
2023-02-17 00:16:46 +0900

    [Feat] 비밀번호 찾기 기능 구현 #55

--- src/main/java/com/saemoim/controller/EmailController.java
+++ src/main/java/com/saemoim/controller/EmailController.java
@@ -4,11 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saemoim.dto.request.EmailRequestDto;
 import com.saemoim.dto.response.EmailAuthCodeResponseDto;
+import com.saemoim.dto.response.MessageResponseDto;
 import com.saemoim.service.EmailServiceImpl;
 
 import jakarta.mail.MessagingException;
@@ -26,4 +28,11 @@ public class EmailController {
 		return new ResponseEntity<>(new EmailAuthCodeResponseDto(authCode), HttpStatus.OK);
 	}
 
+	// 비밀번호 찾기
+	@PutMapping("/email/password")
+	public ResponseEntity<MessageResponseDto> sendTempPassword(
+		@RequestBody @Validated EmailRequestDto emailRequestDto) throws MessagingException {
+		emailService.sendTempPasswordAndChangePassword(emailRequestDto.getEmail());
+		return new ResponseEntity<>(new MessageResponseDto("해당 이메일로 임시번호가 발송되었습니다."), HttpStatus.OK);
+	}
 }
--- src/main/java/com/saemoim/service/EmailServiceImpl.java
+++ src/main/java/com/saemoim/service/EmailServiceImpl.java
@@ -1,9 +1,16 @@
 package com.saemoim.service;
 
 import java.util.Random;
+import java.util.UUID;
 
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.saemoim.domain.User;
+import com.saemoim.exception.ErrorCode;
+import com.saemoim.repository.UserRepository;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
@@ -12,8 +19,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 	private final JavaMailSender emailSender;
 	private String authCode;
+	private String tempPassword;
 
 	public void createCode() {
 
@@ -50,10 +60,48 @@ public class EmailServiceImpl implements EmailService {
 		return message;
 	}
 
+	public void createTempPassword() {
+		UUID uid = UUID.randomUUID();
+		tempPassword = uid.toString().substring(0, 10);
+	}
+
+	public MimeMessage createTempPasswordEmailForm(String email) throws MessagingException {
+		createTempPassword();
+
+		String fromEmail = "dpevent@naver.com";
+		String toEmail = email;
+		String title = "새모임 임시 비밀번호 안내 이메일입니다.";
+		String content = "안녕하세요. 새모임 임시 비밀번호를 발급해드립니다.\n 회원님의 임시 비밀번호는 " + tempPassword + " 입니다.\n" +
+			"로그인 후에 비밀번호를 변경해주세요.";
+
+		MimeMessage message = emailSender.createMimeMessage();    // 마임 메세지 -
+		message.addRecipients(MimeMessage.RecipientType.TO, toEmail);    //
+		message.setSubject(title);    // 메일 제목
+		message.setFrom(fromEmail);    // 메일을 보내는 주체(이메일) 설정
+		message.setText(content);    // 메일 내용 설정. "utf-8" , "html" 등으로 추가 설정 가능
+
+		return message;
+	}
+
 	@Override
 	public String sendEmail(String toEmail) throws MessagingException {
 		MimeMessage emailForm = createEmailForm(toEmail);
 		emailSender.send(emailForm);
 		return authCode;
+	}
+
+	@Transactional
+	@Override
+	public void sendTempPasswordAndChangePassword(String email) throws MessagingException {
+		User user = userRepository.findByEmail(email).orElseThrow(
+			() -> new IllegalArgumentException(ErrorCode.NOT_FOUND_USER.getMessage())
+		);
+
+		MimeMessage emailForm = createTempPasswordEmailForm(email);
+		emailSender.send(emailForm);
+
+		String encodingTempPwd = passwordEncoder.encode(tempPassword);
+		user.updatePassword(encodingTempPwd);
+		userRepository.save(user);
 	}
 }
