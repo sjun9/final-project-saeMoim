fef7b0c00a31fe670763d1627945e6a3e48fdbb1
pooca <pooca12@gmail.com>
2023-02-22 13:54:00 +0900

    [Feat] 웰컴 페이지 연결 - 필드validation 조건 수정 및 메세지 추가#74

--- src/main/java/com/saemoim/dto/request/SignUpRequestDto.java
+++ src/main/java/com/saemoim/dto/request/SignUpRequestDto.java
@@ -1,6 +1,7 @@
 package com.saemoim.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,12 +15,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequestDto {
 	@Email
+	@NotBlank
 	private String email;
 
-	@Size(min = 8, max = 15)
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$")
+	@Size(min = 8, max = 15, message = "최소 8자, 최대 15글자까지 입력 가능합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$" , message = "영문, 숫자, 특수문자를 혼합 해주세요.")
 	private String password;
 
-	@Size(min = 2, max = 10)
+	@Size(min = 2, max = 15, message = "최소 2자, 최대 15글자까지 입력 가능합니다.")
 	private String username;
 }
