12e09fcff77e8af9ae98c37ec9d7aff97f4e8f39
SongeonLee <qkfka1040@icloud.com>
2023-02-22 21:07:56 +0900

    [Comment] 유효성검사 메세지 내용 수정

--- src/main/java/com/saemoim/dto/request/SignUpRequestDto.java
+++ src/main/java/com/saemoim/dto/request/SignUpRequestDto.java
@@ -18,10 +18,10 @@ public class SignUpRequestDto {
 	@NotBlank
 	private String email;
 
-	@Size(min = 8, max = 15, message = "최소 8자, 최대 15글자까지 입력 가능합니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$" , message = "영문, 숫자, 특수문자를 혼합 해주세요.")
+	@Size(min = 8, max = 15, message = "8자 이상 15자 이하여야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]*$" , message = "영문 대/소문자, 숫자, 특수문자를 혼합 해주세요.")
 	private String password;
 
-	@Size(min = 2, max = 15, message = "최소 2자, 최대 15글자까지 입력 가능합니다.")
+	@Size(min = 2, max = 15, message = "2자 이상 15자 이하여야 합니다.")
 	private String username;
 }
