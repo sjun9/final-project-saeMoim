c92574d99ceafa61026e4ba47d08e5f446f4481a
seongjun <seongjuni1@naver.com>
2023-04-03 18:57:15 +0900

    [feat] Hibernate 쿼리 개수 세기

---
+++ src/main/java/com/saemoim/hibernate/ApiQueryCounter.java
@@ -0,0 +1,18 @@
+package com.saemoim.hibernate;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import lombok.Getter;
+
+@Component
+@RequestScope
+@Getter
+public class ApiQueryCounter {
+
+	private int count;
+
+	public void increaseCount() {
+		count++;
+	}
+}
