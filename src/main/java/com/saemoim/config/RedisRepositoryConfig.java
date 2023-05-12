47685c16596a111d8bc408afbdca598dbd3048bc
seongjun <seongjuni1@naver.com>
2023-05-13 08:54:07 +0900

    [feat] redis session, cache 서버 분리

--- src/main/java/com/saemoim/config/RedisRepositoryConfig.java
+++ src/main/java/com/saemoim/config/RedisRepositoryConfig.java
@@ -3,6 +3,7 @@ package com.saemoim.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,13 +11,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisRepositoryConfig {
-
-	@Value("${spring.data.redis.host}")
+	@Value("${spring.data.redis.session.host}")
 	private String host;
-	@Value("${spring.data.redis.port}")
+	@Value("${spring.data.redis.session.port}")
 	private int port;
 
 	@Bean
+	@Primary
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(host, port);
 	}
